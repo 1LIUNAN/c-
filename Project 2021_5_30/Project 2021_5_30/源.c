71fdc8968fd4878dc305107019bcdd0bc301e05b
liunan <1549353679@qq.com>
2021-05-30 11:16:00 +0800

    有一点简单的调试

---
+++ Project 2021_5_30/Project 2021_5_30/源.c
@@ -0,0 +1,67 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int day;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//		printf("星期一\n"); break;
+//	case 2:
+//		printf("星期二\n"); break;
+//	case 3:
+//		printf("星期三\n"); break;
+//	case 4:
+//		printf("星期四\n"); break;
+//	case 5:
+//		printf("星期五\n"); break;
+//	case 6:
+//		printf("星期六\n"); break;
+//	case 7:
+//		printf("星期日\n"); break;
+//
+//	}
+//	return 0;
+//}
+
+//简单的调试
+//int main()
+//{
+//	int a;
+//	for (a = 1; a < 10; a++)
+//		printf("%d\n", a);
+//	return 0;
+//}
+
+//在屏幕上打印1-10
+//int main()
+//{
+//int a = 1;
+//while (a <= 10)
+//{
+//	if (a == 4)
+//	{
+//		a++;
+//		continue;//break;
+//	}
+//	
+//	printf("%d\n", a);
+//	a++;
+//}
+//return 0;
+//}
+
+int main()
+{
+	int a ;
+	while ((scanf("%d",&a))!=EOF)
+
+	{
+		
+		printf("%d\n", a);
+		
+	}
+	return 0;
+}
+
